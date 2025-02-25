cdec1808330f5d9cc63da122432881cf0f56897c
Robin Bezak <76775905+bezakrobin@users.noreply.github.com>
2025-02-25 05:12:07 +0100

    coded F_FormInputLabel.tsx

--- frontend/src/pages/SignInPage.tsx
+++ frontend/src/pages/SignInPage.tsx
@@ -4,6 +4,7 @@ import { F_AuthImageBox } from "../components/F_AuthImageBox.tsx";
 import { F_AuthFormLayout } from "../layouts/F_AuthFormLayout.tsx";
 import { F_FormTitle } from "../components/F_FormTitle.tsx";
 import { F_FormSubtitle } from "../components/F_FormSubtitle.tsx";
+import { F_FormInputLabel } from "../components/F_FormInputLabel.tsx";
 
 
 
@@ -15,6 +16,8 @@ export const SignInPage: React.FC = () => {
                 <F_AuthFormLayout>
                     <F_FormTitle title="Sign In" />
                     <F_FormSubtitle subtitle="Please enter your credentials to access your account" />
+                    <F_FormInputLabel label="Username" />
+                    <F_FormInputLabel label="Password" />
                 </F_AuthFormLayout>
             }
         />
