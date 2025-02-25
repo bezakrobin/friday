f82dd36fa7cdc48a1da4a5221036b7b78d32ce06
Robin Bezak <76775905+bezakrobin@users.noreply.github.com>
2025-02-25 04:30:16 +0100

    added F_AuthFormLayout.tsx

--- frontend/src/pages/SignInPage.tsx
+++ frontend/src/pages/SignInPage.tsx
@@ -1,10 +1,19 @@
 import React from "react";
 import { F_AuthLayout } from "../layouts/F_AuthLayout.tsx";
 import { F_AuthImageBox } from "../components/F_AuthImageBox.tsx";
+import {F_AuthFormLayout} from "../layouts/F_AuthFormLayout.tsx";
+
 
 
 export const SignInPage: React.FC = () => {
     return (
-        <F_AuthLayout leftComponent={ <F_AuthImageBox /> } rightComponent={ <h1>Hi</h1> } />
+        <F_AuthLayout
+            leftComponent={ <F_AuthImageBox /> }
+            rightComponent={
+                <F_AuthFormLayout>
+                    <h1>Hello</h1>
+                </F_AuthFormLayout>
+            }
+        />
     );
 };
