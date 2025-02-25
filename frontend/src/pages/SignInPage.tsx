f8788eafd22bf85fded12d44ee6f5ed5d8eeeaba
Robin Bezak <76775905+bezakrobin@users.noreply.github.com>
2025-02-25 03:20:46 +0100

    added updater back

--- frontend/src/pages/SignInPage.tsx
+++ frontend/src/pages/SignInPage.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export const SignInPage: React.FC = () => {
     return (
         <>
-            SignInPage
+            SignInPage, Update test #1
         </>
     );
 };
