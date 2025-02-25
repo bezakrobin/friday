611d81c218220689b1d6c9cf947be476dde1d430
Robin Bezak <76775905+bezakrobin@users.noreply.github.com>
2025-02-25 03:13:38 +0100

    updated frontend with routing and sign in empty page

---
+++ frontend/src/pages/SignInPage.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const SignInPage: React.FC = () => {
+    return (
+        <>
+            SignInPage
+        </>
+    );
+};
---
+++ frontend/src/routing/Routing.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import { SignInPage } from "../pages/SignInPage.tsx";
+
+export const Routing: React.FC = () => {
+    return (
+        <Router>
+            <Routes>
+                <Route path={"/"} element={ <Navigate to={"/sign-in"} /> } />
+                <Route path={"/sign-in"} element={<SignInPage />} />
+            </Routes>
+        </Router>
+    );
+};
