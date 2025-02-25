7f8eb67d0ef3c628aeae49b37cc7b50b55145645
Robin Bezak <76775905+bezakrobin@users.noreply.github.com>
2025-02-25 03:43:45 +0100

    updated updater and added sign in layout

--- frontend/src/pages/SignInPage.tsx
+++ frontend/src/pages/SignInPage.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-import {F_SignInLayout} from "../layouts/F_SignInLayout.tsx";
+import { F_SignInLayout } from "../layouts/F_SignInLayout.tsx";
 
 export const SignInPage: React.FC = () => {
     return (
-        <F_SignInLayout leftComponent={ <></> } rightComponent={ <></> } />
+        <F_SignInLayout leftComponent={ <h1>Hello</h1> } rightComponent={ <h1>Hi</h1> } />
     );
 };
